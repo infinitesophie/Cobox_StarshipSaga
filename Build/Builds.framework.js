06b1c79126a557cfd40cee4bf5e84ae5e443587f
Sophia Mary <i.sophiamary@gmail.com>
2024-12-25 15:28:49 +0100

    Add Unity WebGL build

---
+++ Build/Builds.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:382232d620feb8948da81a7a0fa7a6b0d5faadaa78d59c775f165eb642e4ead3
+size 343176
---
+++ Build/Webgl.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a823101c65ec1345682b1926dc20ce5f1173e275c389ca3027bd6ac22d43774
+size 343310
